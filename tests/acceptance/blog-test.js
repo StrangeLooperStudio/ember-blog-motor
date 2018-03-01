c607d8dc4c33daec13dd7bd6d5b272d6ebd90d51
Matt Marcum <matt@strangelooper.com>
2018-02-28 18:44:08 -0600

    tests

---
+++ tests/acceptance/blog-test.js
@@ -0,0 +1,26 @@
+import { module, test } from 'qunit';
+import { currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+import page from 'dummy/tests/pages/blog';
+import defaultScenario from 'dummy/mirage/scenarios/default';
+
+module('Acceptance | blog', function(hooks) {
+  setupApplicationTest(hooks);
+
+  test('visiting /blog', async function(assert) {
+
+    await page.visit();
+    assert.equal(currentURL(), '/blog');
+    assert.ok(page.previous);
+    assert.notOk(page.nextLink);
+  });
+
+  test('visiting previous blog post', async function(assert) {
+    defaultScenario(window.server);
+    await page.visit();
+    await page.previousLink();
+    assert.ok(/^\/blog\/posts\/(.*)$/.test(currentURL()));
+    assert.ok(page.previous);
+    assert.ok(page.nextLink);
+  });
+});
--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,4 +1,10 @@
-import resolver from './helpers/resolver';
-import { setResolver } from 'ember-mocha';
+import Application from '../app';
+import config from '../config/environment';
+import { setApplication } from '@ember/test-helpers';
+import { start } from 'ember-qunit';
+import preloadAssets from 'ember-asset-loader/test-support/preload-assets';
+import manifest from 'dummy/config/asset-manifest';
 
-setResolver(resolver);
+setApplication(Application.create(config.APP));
+
+preloadAssets(manifest).then(start);
