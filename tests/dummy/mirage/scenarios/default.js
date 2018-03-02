0b2d15d8ddef10bcadcca088795fc3ac5d2ac3ed
Matt Marcum <matt@strangelooper.com>
2018-03-02 17:32:20 -0600

    start blog posts

--- tests/dummy/mirage/scenarios/default.js
+++ tests/dummy/mirage/scenarios/default.js
@@ -12,10 +12,10 @@ import moment from 'moment';
 
 export default function(server) {
 
-  let alice = server.create('user', { isAdmin: true, email: 'mm@ebm.com', name: 'Matt Marcum'});
-  let bob = server.create('user', { id: 2, isAdmin: false, email: 'bob@ebm.com', name: 'Bob' });
+  // let alice = server.create('user', { id: 1, isAdmin: true, email: 'mm@ebm.com', name: 'Matt Marcum' });
+  // let bob = server.create('user', { id: 2, isAdmin: false, email: 'bob@ebm.com', name: 'Bob' });
 
-  server.loadFixtures('posts');
+  server.loadFixtures();
 
   // for(let i=0; i<99; i++) {
   //   let author = i%2 ? alice : bob;
